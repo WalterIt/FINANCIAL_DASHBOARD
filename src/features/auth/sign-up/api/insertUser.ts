8447594611f6b88d38c4ac833edb4ee40c9e4b0a
WalterIt <wshiruba@gmail.com>
2024-06-27 14:23:25 -0400

    IMPLEMENTED: Register (Sign-Up) API & UI with Bcryptjs

--- src/features/auth/sign-up/api/insertUser.ts
+++ src/features/auth/sign-up/api/insertUser.ts
@@ -3,11 +3,18 @@
 // src/lib/insertUser.ts
 import db from "@/db/drizzle";
 import { NewUser, User, users } from "@/db/schema";
+import bcrypt from "bcryptjs"
 
 export async function insertUser(user: NewUser): Promise<User[]> {
-  console.log(`Inserting user: ${JSON.stringify(user)}`);
-  try {
-    return await db.insert(users).values(user).returning();
+  const { name, email, password } = user;
+  let hashedPassword: string | null = null;
+  
+  if (password) {
+    hashedPassword = await bcrypt.hash(password, 10);
+  }
+  
+  try {    
+    return await db.insert(users).values({...user, password: hashedPassword}).returning();
   } catch (error) {
     if ((error as { message: string }).message.includes("users_email_unique")) {
       throw new Error("Email is already used");
